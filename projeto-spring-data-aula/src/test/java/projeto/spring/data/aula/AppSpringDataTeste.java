a26467a64331144f9f02304d9240ffc5aead6908
Luan Pinheiro <lluanps@gmail.com>
2023-01-24 23:47:50 -0300

    Povoando BD

--- projeto-spring-data-aula/src/test/java/projeto/spring/data/aula/AppSpringDataTeste.java
+++ projeto-spring-data-aula/src/test/java/projeto/spring/data/aula/AppSpringDataTeste.java
@@ -2,16 +2,37 @@ package projeto.spring.data.aula;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import projeto.spring.data.aula.dao.InterfaceSpringDataUser;
+import projeto.spring.data.aula.model.UsuarioSpringData;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:META-INF/spring-config.xml"})
 public class AppSpringDataTeste {
 	
+	@Autowired
+	private InterfaceSpringDataUser interfaceSpringDataUser;
+	
 	@Test
 	public void testeInsert() {
-		System.out.println("Iniciou spring com sucesso");
+		
+		UsuarioSpringData entity = new UsuarioSpringData();
+		entity.setEmail("anab@gmail.com");
+		entity.setIdade(18);
+		entity.setLogin("teste 243");
+		entity.setSenha("000");
+		entity.setNome("Ana Bittencurt");
+		
+		interfaceSpringDataUser.save(entity);
 	}
 
+	@Test
+	public void testeConsulta() {
+		System.out.println("Iniciou spring com sucesso");
+	}
+	
+	
 }
